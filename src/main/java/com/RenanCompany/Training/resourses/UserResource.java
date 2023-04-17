94997d8b7df8c0498d4decad46c4771e457838ab
Renan Reis <renanmacfernandes@hotmail.com>
2023-04-16 21:35:23 -0300

    service application

--- src/main/java/com/RenanCompany/Training/resourses/UserResource.java
+++ src/main/java/com/RenanCompany/Training/resourses/UserResource.java
@@ -1,21 +1,33 @@
 package com.RenanCompany.Training.resourses;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.RenanCompany.Training.entity.User;
+import com.RenanCompany.Training.services.UserService;
 
 @RestController
 @RequestMapping(value ="/Users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		
-		User u = new User(1L, "Renan", "renan@email.com", "9999999", "1234");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User>findById(Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
